ce069f846bd284a31a61777f9a73fcd4e035c0e1
juhyunham <judev0227@gmail.com>
2022-02-06 17:35:26 +0900

    icon button update

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -16,30 +16,11 @@ class MyApp extends StatelessWidget {
             '앱임',
           ),
         ),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            width: double.infinity,
-            height: 50,
-            margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-              border: Border.all(color: Colors.black),
-            )
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          ),
+        body: SizedBox(
+          child: IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          )
         )
       )
     );
