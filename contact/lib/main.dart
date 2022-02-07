653a6e041c933cee65b33da63aa9d415aeefea04
juhyunham <judev0227@gmail.com>
2022-02-07 11:29:51 +0900

    listView, custom Widget update

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -12,20 +12,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: ShopItem()
+        body: ListView(
+          children: [
+            Row(
+              children: [
+                Icon(Icons.sentiment_satisfied_alt),
+                Text("홍길동")
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.sentiment_satisfied_alt),
+                Text("홍길동")
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.sentiment_satisfied_alt),
+                Text("홍길동")
+              ],
+            )
+          ]
+        ),
+        bottomNavigationBar: BottomNavBar(),
       )
     );
   }
 }
 
-
-class ShopItem extends StatelessWidget {
-  const ShopItem({ Key? key }) : super(key: key);
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-          child: Text("안녕 커스텀 위젯?")
-        );
+    return  BottomAppBar (
+      child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(icon: Icon(Icons.phone), onPressed: () {}),
+              IconButton(icon: Icon(Icons.message), onPressed: () {}),
+              IconButton(icon: Icon(Icons.contact_page), onPressed: () {})
+            ],
+        )
+      )
+      
+    );
   }
 }
